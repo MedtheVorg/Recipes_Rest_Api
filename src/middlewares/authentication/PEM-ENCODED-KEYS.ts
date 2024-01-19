85601142b8c5cf9669a3d4a2d29fba6f7f121120
MEDPC <mohamedlem216@gmail.com>
2024-01-19 18:02:18 +0100

    added a function to generate and store jwt  hashing keys in the keys folder

--- src/middlewares/authentication/PEM-ENCODED-KEYS.ts
+++ src/middlewares/authentication/PEM-ENCODED-KEYS.ts
@@ -1,22 +1,32 @@
 import { generateKeyPairSync } from 'crypto';
 import fs from 'fs';
-// Generate a key pair to be used to work with the RS256 Algorithm
-// const { publicKey, privateKey } = generateKeyPairSync('rsa', {
+
+/**
+ * uncomment this function to generate and store RS256 algorithm key pairs
+ * in the keys folder
+ */
+// const { privateKey, publicKey } = generateKeyPairSync('rsa', {
 //   modulusLength: 2048,
 //   publicKeyEncoding: { type: 'spki', format: 'pem' },
 //   privateKeyEncoding: { type: 'pkcs8', format: 'pem' },
 // });
+// fs.writeFileSync(`${__dirname}/../../keys/private_key.pem`, privateKey, {
+//   encoding: 'utf8',
+// });
+// fs.writeFileSync(`${__dirname}/../../keys/public_key.pem`, publicKey, {
+//   encoding: 'utf8',
+// });
 
 // Private Key: A secret key used in asymmetric cryptography. It must be securely stored and is used to generate digital signatures and decrypt data.
 // used to sign Json web tokens
-const privateKey = fs.readFileSync(`${__dirname}/../../keys/private_key.pem`, {
+const PRIV_KEY = fs.readFileSync(`${__dirname}/../../keys/private_key.pem`, {
   encoding: 'utf8',
 });
 
 // Public Key: A key derived from the private key, meant for distribution. It's used to verify digital signatures and encrypt data that only the private key holder can decrypt.
 // used to verify Json web tokens
-const publicKey = fs.readFileSync(`${__dirname}/../../keys/public_key.pem`, {
+const PUB_KEY = fs.readFileSync(`${__dirname}/../../keys/public_key.pem`, {
   encoding: 'utf8',
 });
 
-export { publicKey as PUB_KEY, privateKey as PRIV_KEY };
+export { PUB_KEY, PRIV_KEY };
