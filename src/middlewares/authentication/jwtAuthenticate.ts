645bd04b87679f0e1d32a58709696219a5cf0fc2
MEDPC <mohamedlem216@gmail.com>
2024-01-19 18:14:43 +0100

    added some comments

--- src/middlewares/authentication/jwtAuthenticate.ts
+++ src/middlewares/authentication/jwtAuthenticate.ts
@@ -3,6 +3,9 @@ import passport from 'passport';
 import { HttpCode } from '../../utils/AppError';
 
 function jwtAuthentication(req: Request, res: Response, next: NextFunction) {
+  // this function will fetch the jwt token from the Authentication header
+  // in the form of Bearer <token> and will pass it to the jwt strategy verify function
+  // to validate
   passport.authenticate(
     'jwt',
     { session: false },
